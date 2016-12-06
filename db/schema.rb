10dd700ce7740ef7f93b40ff9fdaaa18224d6dba
Heitor <hagalmeida@inf.ufpel.edu.br>
2016-12-06 01:00:28 -0200

    add association between user and articles and add debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206022631) do
+ActiveRecord::Schema.define(version: 20161206024832) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
